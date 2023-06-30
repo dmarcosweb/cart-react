c9802ec4a0d265f34732c11410bb72126b0e3ca7
Daniel mpm <dmarcosmpm.js@outlook.com>
2023-06-30 16:21:04 -0300

    →Implementation display ←

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,7 +3,7 @@ const Navbar = () => {
   return (
     <nav>
       <div className='nav-center'>
-        <h4>useReducer</h4>
+        <h4>Store cart example</h4>
         <div className='nav-container'>
           <FaCartPlus className='cart-icon' />
           <div className='amount-container'>
